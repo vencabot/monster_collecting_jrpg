d059ea7a7c53c6293a22092bd82bf0f61db7560d
vencabot <ifyoudieinthegameyoudieforreal@gmail.com>
2019-04-25 20:06:43 -0400

    Add files via upload

--- mockups/battle_4.py
+++ mockups/battle_4.py
@@ -1,13 +1,122 @@
 import random
 
-import dynamic_system_4
+class DynamicObject:
+    def __init__(self, ruleset):
+        self.ruleset = ruleset
+        
+    def update_w_rules(
+            self, attr_name, new_value, perpetrated_by=None,
+            w_ability=None, w_rule=None):
+        old_value = self.__dict__[attr_name]
+        dynamic_event = DynamicEvent(
+                self, attr_name, new_value, old_value, perpetrated_by,
+                w_ability, w_rule)
+        if new_value == old_value:
+            return
+        final_event = self.ruleset.run_through_before_phase(
+                dynamic_event)
+        self.__dict__[attr_name] = final_event.new_value
+        self.ruleset.run_after_phase_for(final_event)
+
+
+class DynamicRule:
+    def __init__(self, rule_name, check_phase, initiator, w_ability):
+        self.rule_name = rule_name
+        self.recurrence_counter = 0
+        self.recurrence_limit = 1
+        self.check_phase = check_phase
+        self.initiator = initiator
+        self.w_ability = w_ability
+
+    def react_to(self, dynamic_event):
+        if self.recurrence_counter == self.recurrence_limit:
+            self.at_limit(dynamic_event)
+        elif self.will_trigger_on(dynamic_event):
+            self.recurrence_counter += 1
+            self.trigger(dynamic_event)
+
+    def will_trigger_on(self, dynamic_event):
+        return False
+
+    def trigger(self, dynamic_event):
+        pass
+
+    def at_limit(self, dynamic_event):
+        #print(f"{self.rule_name} has reached its limit.")
+        pass
+
+    def fail(self, dynamic_event):
+        print(f"{self.rule_name} failed to trigger.")
+
+
+class Ruleset(DynamicObject):
+    def __init__(self, before_rules=[], after_rules=[]):
+        super().__init__(self)
+        self.rules = before_rules + after_rules
+
+    def run_through_before_phase(self, dynamic_event):
+        before_rules = []
+        for dynamic_rule in self.rules:
+            if dynamic_rule.check_phase == "before":
+                before_rules.append(dynamic_rule)
+        for dynamic_rule in before_rules:
+            dynamic_rule.react_to(dynamic_event)
+            dynamic_event = dynamic_event.get_newest_event()
+        return dynamic_event
+
+    def run_after_phase_for(self, dynamic_event):
+        after_rules = []
+        for dynamic_rule in self.rules:
+            if dynamic_rule.check_phase == "after":
+                after_rules.append(dynamic_rule)
+        for dynamic_rule in after_rules:
+            dynamic_rule.react_to(dynamic_event)
+
+    def reset_recurrence_counters(self):
+        for dynamic_rule in self.rules:
+            dynamic_rule.recurrence_counter = 0
+        print("DIAGNOSTIC: dynamic recurrence counters have been reset")
+
+# FIX THIS TODAY
+class DynamicEvent:
+    def __init__(
+            self, target, attr_name, new_value, old_value,
+            perpetrated_by, w_ability=None, w_rule=None, replaces=None):
+        self.target = target
+        self.attr_name = attr_name
+        self.new_value = new_value
+        self.old_value = old_value
+        self.perpetrated_by = perpetrated_by
+        self.w_ability = w_ability
+        self.w_rule = w_rule
+        self.replaces = replaces
+        self.replaced_by = None
+
+    def replace_value(self, new_value, perpetrated_by):
+        self.replaced_by = DynamicEvent(
+                self.target, self.attr_name, new_value, self.old_value,
+                perpetrated_by, self.ability, self)
+
+    def get_original_event(self):
+        original_event = self
+        while original_event.replaces is not None:
+            original_event = original_event.replaces
+        return original_event
+
+    def get_newest_event(self):
+        newest_event = self
+        while newest_event.replaced_by is not None:
+            newest_event = newest_event.replaced_by
+        return newest_event
+
 
 class Battle:
     def __init__(self):
-        self.ruleset = dynamic_system_4.Ruleset()
+        self.ruleset = Ruleset()
+        self.leaders = []
 
 
-class BattleLeader(dynamic_system_4.DynamicObject):
+class BattleLeader(DynamicObject):
     def __init__(self, ruleset, leader_name):
         super().__init__(ruleset)
         self.leader_name = leader_name
@@ -16,7 +125,7 @@ class BattleLeader(dynamic_system_4.DynamicObject):
         self.ap = self.max_ap
 
 
-class BattleUnit(dynamic_system_4.DynamicObject):
+class BattleUnit(DynamicObject):
     def __init__(self, ruleset, unit_name):
         super().__init__(ruleset)
         self.unit_name = unit_name
--- mockups/rules_and_events_4.py
+++ mockups/rules_and_events_4.py
@@ -1,81 +1,76 @@
 import battle_4
-import dynamic_system_4
 
 class Slap(battle_4.UnitAbility):
     ability_name = "Slap"
 
     @classmethod
-    def use_glancing(cls, perpetrator, targets, battle):
+    def deal_some_damage(cls, perpetrator, target, battle, damage):
         for leader in battle.leaders:
-            if perpetrator in leader.units:
+            if perpetrator in leader.party:
                 perp_leader = leader
                 break
-        print(f"{perpetrator.unit_name} slapped {targets[0].unit_name}!")
-        targets[0].update_w_rules("hp", targets[0].hp - 1, cls)
-        perpetrator.update_w_rules("mp", perpetrator.mp - 1, cls)
-        perp_leader.update_w_rules("ap", perp_leader.ap - 2, cls)
+        print(f"{perpetrator.unit_name} slapped {target.unit_name}!")
+        target.update_w_rules(
+                "hp", target.hp - damage, perpetrator, cls)
+        perpetrator.update_w_rules(
+                "mp", perpetrator.mp - 1, perpetrator, cls)
+        perp_leader.update_w_rules(
+                "ap", perp_leader.ap - 2, perpetrator, cls)
+
+    @classmethod
+    def use_glancing(cls, perpetrator, targets, battle):
+        cls.deal_some_damage(perpetrator, targets[0], battle, 1)
 
     @classmethod
     def use_normal(cls, perpetrator, targets, battle):
-        leader = self.owner.party.leader
-        print(f"{self.owner.unit_name} slapped {targets[0].unit_name}!")
-        targets[0].hp.update(targets[0].hp.value - 2, self)
-        self.owner.mp.update(self.owner.mp.value - 1, self)
-        leader.ap.update(leader.ap.value - 2, self)
+        cls.deal_some_damage(perpetrator, targets[0], battle, 2)
 
     @classmethod
     def use_critical(cls, perpetrator, targets, battle):
-        leader = self.owner.party.leader
-        print(f"{self.owner.unit_name} slapped {targets[0].unit_name}!")
-        targets[0].hp.update(targets[0].hp.value - 4, self)
-        self.owner.mp.update(self.owner.mp.value - 1, self)
-        leader.ap.update(leader.ap.value - 2, self)
+        cls.deal_some_damage(perpetrator, targets[0], battle, 4)
 
+
+class Rage(battle_4.DynamicRule):
+    def __init__(self, target_unit):
+        super().__init__("Rage", "after", None, None)
+        self.target_unit = target_unit
+
+    def will_trigger_on(self, dynamic_event):
+        if (
+                dynamic_event.target is self.target_unit
+                and dynamic_event.attr_name == "hp"
+                and dynamic_event.new_value < dynamic_event.old_value):
+            return True
+        return False
+
+    def trigger(self, dynamic_event):
+        print(
+                f"{self.target_unit.unit_name} didn't like that very "
+                "much! >:O")
+        self.target_unit.update_w_rules(
+            "atk", self.target_unit.atk + 1,
+            dynamic_event.perpetrated_by, dynamic_event.w_ability, self)
 
 
 if __name__ == "__main__":
-    our_ruleset = dynamic_system_4.Ruleset()
-    vencabot = battle_4.BattleUnit(our_ruleset, "Vencabot")
-    kreichjr = battle_4.BattleUnit(our_ruleset, "KReichJr")
     our_battle = battle_4.Battle()
 
-    party_a = battle_4.BattleParty("party_a")
-    our_battle.append_party(party_a)
-    vencabot = battle_4.BattleLeader("Vencabot")
-    kd_alpha = battle_4.BattleUnit("KD_Alpha")
-    mexi = battle_4.BattleUnit("Mexi")
-    party_a.append_leader(vencabot)
-    party_a.append_unit(kd_alpha)
-    party_a.append_unit(mexi)
-    party_a.point_unit.update(kd_alpha, vencabot)
+    goodvibe = battle_4.BattleLeader(our_battle.ruleset, "GoodVibe")
+    vencabot = battle_4.BattleUnit(our_battle.ruleset, "Vencabot")
+    goodvibe.party.append(vencabot)
+    our_battle.leaders.append(goodvibe)
 
-    party_b = battle_4.BattleParty("party_b")
-    our_battle.append_party(party_b)
-    kreichjr = battle_4.BattleLeader("KReichJr")
-    goodvibe = battle_4.BattleUnit("GoodVibe")
-    slade = battle_4.BattleUnit("zxxsladexxz")
-    party_b.append_leader(kreichjr)
-    party_b.append_unit(goodvibe)
-    party_b.append_unit(slade)
-    party_b.point_unit.update(goodvibe, kreichjr)
-    goodvibe.learn_ability(Slap)
+    penguinpowered = battle_4.BattleLeader(
+            our_battle.ruleset, "PenguinPowered")
+    kreichjr = battle_4.BattleUnit(our_battle.ruleset, "KReichJr")
+    penguinpowered.party.append(kreichjr)
+    our_battle.leaders.append(penguinpowered)
 
-    our_battle.append_rule(BloodLust(goodvibe))
-    our_battle.append_rule(Hench(goodvibe))
-    our_battle.append_rule(ExtraDamage(goodvibe))
-    our_battle.append_rule(Rage(kd_alpha))
-    our_battle.append_rule(Persistence(goodvibe))
-    our_battle.append_rule(Invincible(kd_alpha))
-    our_battle.append_rule(MagicMan(goodvibe))
-    our_battle.append_rule(OldManGenes(kreichjr))
+    our_battle.ruleset.update_w_rules(
+            "rules", our_battle.ruleset.rules + [Rage(kreichjr)])
 
-    our_battle.active_party.update(party_a, our_battle)
-    our_battle.next_turn()
-
-    party_b.point_unit.value.abilities["Slap"][0].use([kd_alpha])
-    print()
-    party_b.point_unit.value.abilities["Slap"][0].use([kd_alpha])
-    print()
-    party_b.point_unit.value.abilities["Slap"][0].use([kd_alpha])
-    print()
-    party_b.point_unit.value.abilities["Slap"][0].use([kd_alpha])
+    print(f"KReich's HP is at {kreichjr.hp}.")
+    print(f"KReich's ATK is at {kreichjr.atk}.")
+    vencabot.use(Slap, [kreichjr], our_battle)
+    print(f"KReich's HP is at {kreichjr.hp}.")
+    print(f"KReich's ATK is at {kreichjr.atk}.")
