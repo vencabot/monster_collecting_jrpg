c7917e051ab6de238494f1edf6f12fb1bb905947
vencabot <ifyoudieinthegameyoudieforreal@gmail.com>
2019-05-16 19:12:09 -0400

    Add files via upload

---
+++ mockups/version_3/battle_3.py
@@ -0,0 +1,110 @@
+import random
+
+import dynamic_system_3
+
+class Battle:
+    def __init__(self):
+        self.parties = []
+        self.dynamic_rules = {"before": [], "after": []}
+        self.active_party = dynamic_system_3.DynamicAttribute(
+                self, "active_party", None)
+
+    def append_party(self, party):
+        self.parties.append(party)
+        party.battle = self
+
+    def append_rule(self, rule):
+        self.dynamic_rules[rule.check_phase].append(rule)
+
+    def reset_dynamic_recurrence(self):
+        rules = (
+                self.dynamic_rules["before"]
+                + self.dynamic_rules["after"])
+        for dynamic_rule in rules:
+            dynamic_rule.recurrence_counter = 0
+        print("DIAGNOSTIC: dynamic recurrence counters have been reset")
+
+    def next_turn(self):
+        # This will only work if we're limited to two parties per battle.
+        for party in self.parties:
+            if party is not self.active_party.value:
+                self.active_party.value.leader.ap.update(
+                    self.active_party.value.leader.max_ap.value, self)
+                self.active_party.update(party, self.active_party.value)
+                break
+
+
+class BattleParty:
+    def __init__(self, party_name):
+        self.party_name = party_name
+        self.units = []
+        self.leader = None
+        self.point_unit = dynamic_system_3.DynamicAttribute(
+                self, "point_unit", None)
+        self.battle = None
+
+    def append_unit(self, party_unit):
+        self.units.append(party_unit)
+        party_unit.party = self
+
+    def append_leader(self, party_leader):
+        self.leader = party_leader
+        party_leader.party = self
+
+
+class BattleLeader:
+    def __init__(self, leader_name):
+        self.leader_name = leader_name
+        self.party = None
+        self.max_ap = dynamic_system_3.DynamicAttribute(
+                self, "max_ap", 10)
+        self.ap = dynamic_system_3.DynamicAttribute(
+                self, "ap", self.max_ap.value)
+
+
+class BattleUnit:
+    def __init__(self, unit_name):
+        self.unit_name = unit_name
+        self.hp = dynamic_system_3.DynamicAttribute(self, "hp", 10)
+        self.atk = dynamic_system_3.DynamicAttribute(self, "atk", 5)
+        self.mp = dynamic_system_3.DynamicAttribute(self, "mp", 5)
+        self.abilities = {}
+        self.party = None
+
+    def learn_ability(self, ability_class):
+        new_ability = ability_class(self)
+        if new_ability.ability_name not in self.abilities:
+            self.abilities[new_ability.ability_name] = []
+        self.abilities[new_ability.ability_name].append(new_ability)
+
+
+class UnitAbility:
+    def __init__(self, ability_name, owner):
+        self.ability_name = ability_name
+        self.owner = owner
+
+    def use(self, targets):
+        glancing_chance = 15
+        critical_chance = 15
+        normal_chance = 100 - glancing_chance - critical_chance
+        effectiveness_list = [
+            self._use_glancing, self._use_normal, self._use_critical]
+        effectiveness_weights = [
+            glancing_chance, normal_chance, critical_chance]
+        use_callables = random.choices(
+                effectiveness_list, effectiveness_weights)
+        if use_callables[0] == self._use_glancing:
+            print(f"DIAGNOSTIC: {self.ability_name} was glancing.")
+        elif use_callables[0] == self._use_critical:
+            print(f"DIAGNOSTIC: {self.ability_name} was critical.")
+        use_callables[0](targets)
+        self.owner.party.battle.reset_dynamic_recurrence()
+        
+    def _use_glancing(self, targets):
+        self._use_normal(targets)
+
+    def _use_normal(self, targets):
+        pass
+
+    def _use_critical(self, targets):
+        self._use_normal(targets)
---
+++ mockups/version_3/dynamic_system_3.py
@@ -0,0 +1,96 @@
+import battle_3
+
+class DynamicEvent:
+    def __init__(
+            self, target, attr_name, new_value, old_value,
+            perpetrated_by, replaces=None):
+        self.target = target
+        self.attr_name = attr_name
+        self.new_value = new_value
+        self.old_value = old_value
+        self.perpetrated_by = perpetrated_by
+        self.replaces = replaces
+        self.replaced_by = None
+
+    def replace_value(self, new_value, perpetrated_by):
+        self.replaced_by = DynamicEvent(
+                self.target, self.attr_name, new_value, self.old_value,
+                perpetrated_by, self)
+
+    def get_original_event(self):
+        original_event = self
+        while original_event.replaces is not None:
+            original_event = original_event.replaces
+        return original_event
+
+class DynamicAttribute:
+    def __init__(self, owner, attr_name, value):
+        self.owner = owner
+        self.attr_name = attr_name
+        self.value = value
+
+    def update(self, new_value, perpetrated_by):
+        old_value = self.value
+        dynamic_event = DynamicEvent(
+                self.owner, self.attr_name, new_value, old_value,
+                perpetrated_by)
+        #print(f"DIAGNOSTIC: Proposed value change to {new_value}.")
+        if new_value == old_value:
+            return
+        print(f"DIAGNOSTIC: {type(self.owner)}")
+        if isinstance(self.owner, battle_3.BattleUnit):
+            battle = self.owner.party.battle
+            owner_name = self.owner.unit_name
+        elif isinstance(self.owner, battle_3.BattleParty):
+            battle = self.owner.battle
+            owner_name = f"{self.owner.leader.leader_name}'s party"
+        elif isinstance(self.owner, battle_3.BattleLeader):
+            battle = self.owner.party.battle
+            owner_name = self.owner.leader_name
+        elif isinstance(self.owner, battle_3.Battle):
+            battle = self.owner
+            owner_name = "da battle"
+        rules = battle.dynamic_rules
+        for dynamic_rule in rules["before"]:
+            dynamic_rule.check(dynamic_event)
+            while dynamic_event.replaced_by is not None:
+                dynamic_event = dynamic_event.replaced_by
+        final_value = dynamic_event.new_value
+        print(
+                f"DIAGNOSTIC: {owner_name}'s {self.attr_name} changed "
+                f"from {self.value} to {final_value}.")
+        self.value = final_value
+        for dynamic_rule in rules["after"]:
+            dynamic_rule.check(dynamic_event)
+
+
+class DynamicRule:
+    def __init__(
+            self, rule_name, check_phase, event_not_prevented=True,
+            event_prevented=False):
+        self.rule_name = rule_name
+        self.recurrence_counter = 0
+        self._recurrence_limit = 1
+        self.check_phase = check_phase
+
+    def check(self, dynamic_event):
+        if self.recurrence_counter >= self._recurrence_limit:
+            self._at_limit(dynamic_event)
+            return False
+        elif self._check(dynamic_event):
+            self.recurrence_counter += 1
+            self._trigger(dynamic_event)
+            return True
+
+    def _check(self, dynamic_event):
+        return False
+
+    def _trigger(self, dynamic_event):
+        pass
+
+    def _at_limit(self, dynamic_event):
+        #print(f"{self.rule_name} has reached its limit.")
+        pass
+
+    def _fail(self, dynamic_event):
+        print(f"{self.rule_name} failed to trigger.")
